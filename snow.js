9aa248f8fec72f7b0be04c71c7015b6b23a9a1fa
Jessica Lowery <jessica.e.lowery@gmail.com>
2016-12-24 21:00:07 -0600

    Upload files

---
+++ snow.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$("body").append("<div class='flake0'></div>");
+	$("body").append("<div class='flake1'></div>");
+	$("body").append("<div class='flake2'></div>");
+	$("body").append("<div class='flake3'></div>");
+	$("body").append("<div class='flake4'></div>");
+	$("body").append("<div class='flake5'></div>");
+	$("body").append("<div class='flake6'></div>");
+	$("body").append("<div class='flake7'></div>");
+	$("body").append("<div class='flake8'></div>");
+	$("body").append("<div class='flake9'></div>");
+
+
+});
